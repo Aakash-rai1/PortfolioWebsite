b00e205e099fef58202709322b7a4c57b341181c
unknown <aakashrie@gmail.com>
2024-01-12 17:38:12 -0500

    final changes

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -51,7 +51,7 @@ const AboutSection = () => {
 
   return (
     <section className="text-white">
-      <div className="md:grid md:grid-cols-2 gap-8 items-center py-12 px-4 xl:gap-16 sm:py-16 xl:px-16">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center mt-20 py-12 px-4 xl:gap-16 sm:py-16 xl:px-16">
         <Image
           src="/images/about.png"
           alt="about image"
--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -8,18 +8,19 @@ const projectsData = [
   {
     id: 1,
     title: "React portfolio",
-    description: "project 1 is here",
-    image: "/images/project1.png",
+    description: "I created this website myself using NextJs as my frontend tool",
+    image: "/images/port.png",
     tag: ["All", "Web"],
     gitUrl: "/",
     previewUrl: "/",
   },
   {
     id: 2,
-    title: "The Aroma Restaurant",
-    description: "project 1 is here",
-    image: "/images/hero-image.png",
-    tag: ["All", "Graphic Design"],
+    title: "CISWP",
+    description:
+      "Associated with Conestoga College, The Canadian Institute for Safety, Wellness & Performance (CISWP) is committed to building productive and sustainable workplaces through a collaborative and transdisciplinary approach. ",
+    image: "/images/cisp.png",
+    tag: ["All", "Web"],
     gitUrl: "/",
     previewUrl: "/",
   },
@@ -35,7 +36,8 @@ const projectsData = [
   {
     id: 4,
     title: "Social Media Posts",
-    description: "Checkerchain is a review-2-earn blockchain platform.",
+    description:
+      "I have created social media banners for various marketing campaigns",
     image: "/images/social.png",
     tag: ["All", "Graphic Design"],
     gitUrl: "/",
@@ -44,11 +46,21 @@ const projectsData = [
   {
     id: 5,
     title: "The Aroma Restaurant",
-    description: "The Aroma is a small restaurant that provides authentic Indian and Hakka cuisine.",
+    description:
+      "The Aroma is a small restaurant that provides authentic Indian and Hakka cuisine.",
     image: "/images/aroma.png",
     tag: ["All", "Graphic Design"],
     gitUrl: "/",
     previewUrl: "https://www.instagram.com/thearomacambridge/",
+  },
+  {
+    id: 6,
+    title: "CISWP Product Design",
+    description: "Along with the development, I was also responsible for designing the wireframes and UI/UX of CISWP.",
+    image: "/images/uicsip.png",
+    tag: ["All", "Product Design"],
+    gitUrl: "/",
+    previewUrl: "https://checkerchain.com",
   },
 ];
 
@@ -92,14 +104,14 @@ const ProjectSection = () => {
           isSelected={tag === "Product Design"}
         />
       </div>
-      <ul  ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
+      <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
         {filteredProjects.map((project, index) => (
           <motion.li
             key={index}
             variants={cardVariants}
             initial="initial"
             animate={isInView ? "animate" : "initial"}
-            transition={{duration: 0.3, delay:index *0.4}}
+            transition={{ duration: 0.3, delay: index * 0.4 }}
           >
             <ProjectCard
               key={project.id}
