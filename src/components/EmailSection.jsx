7be1d51ebaa7692e2bc649db1895c7b87cd9764a
Aakash Chamling Rai <57335739+Aakash-rai1@users.noreply.github.com>
2024-01-12 16:42:40 -0500

    Update EmailSection.jsx

--- src/components/EmailSection.jsx
+++ src/components/EmailSection.jsx
@@ -111,7 +111,7 @@ const EmailSection = () => {
                 id="subject"
                 required
                 className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
-                placeholder="Just saying hi"
+                placeholder="Enter your subject here"
               />
             </div>
             <div className="mb-6">
